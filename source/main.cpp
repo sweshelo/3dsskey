09a417b7768d03d60ecf913190470375a4f3b069
sweshelo <sweshelo@gmail.com>
2023-02-13 17:14:33 +0900

    [delete] unnecesary include

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,7 @@
 #include <3ds.h>
-#include <string>
-#include <array>
-#include <iostream>
 #include <citro2d.h>
-#include <initializer_list>
 
-#include "http.hpp"
-#include "json.hpp"
-#include "qrcodegen.hpp"
-#include "util.hpp"
-#include "Keyboard.hpp"
 #include "Misskey.hpp"
-
-using namespace qrcodegen;
-
-const u32 springgreen = C2D_Color32(0x00, 0xFF, 0x7F, 0xFF);
-const u32 greenyellow = C2D_Color32(0xAD, 0xFF, 0x2F, 0xFF);
-const u32 black = C2D_Color32(0x00, 0x00, 0x00, 0xFF);
-const u32 white = C2D_Color32(0xFF, 0xFF, 0xFF, 0xFF);
 
 int main(int argc, char **argv)
 {
